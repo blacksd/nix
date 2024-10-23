bacc4a37415851dfdab1be2cfd7b778cfe75ee43
Marco Bulgarini <marco.bulgarini@hivemq.com>
2024-10-23 17:53:34 +0200

    feat: zsh candy
    Signed-off-by: Marco Bulgarini <marco.bulgarini@hivemq.com>

--- home/shell.nix
+++ home/shell.nix
@@ -1,34 +1,60 @@
-{pkgs,...}: {
+{pkgs,lib,...}: {
   
   home.packages = with pkgs; [
     zsh
     zsh-powerlevel10k
+    meslo-lgs-nf
     oh-my-zsh
     direnv
   ];
 
+  fonts.fontconfig.enable = true;
+
   programs.zsh = {
     enable = true;
     enableCompletion = true;
+
+    # initExtraBeforeCompInit = ''
+    #   # powerline10k
+    #   source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
+    #   source ${./p10k.zsh}
+    # '';
     initExtra = ''
       export PATH="$PATH:$HOME/.local/bin:$HOME/go/bin"
     '';
+    
     oh-my-zsh = {
       enable = true;
-      theme = "robbyrussell";
       plugins = [ 
         "git" 
         "sudo" 
         "docker" 
         "kubectl"
-        "fzf" 
+        "fzf"
       ];
     };
+
     history = {
       ignoreAllDups = true;
       save = 128000;
       size = 256000;
     };
+
+    # This is another neat way to do it 
+    # https://github.com/NixOS/nixpkgs/issues/154696#issuecomment-1238433989
+    plugins = [
+      {
+        # A prompt will appear the first time to configure it properly
+        name = "powerlevel10k";
+        src = pkgs.zsh-powerlevel10k;
+        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+      }
+      {
+        name = "powerlevel10k-config";
+        src = lib.cleanSource ./configs;
+        file = ".p10k.zsh";
+      }
+    ];
   };
 
   programs.direnv = {
--- modules/system.nix
+++ modules/system.nix
@@ -173,24 +173,24 @@
   time.timeZone = "Europe/Rome";
 
   # Fonts
-  # fonts = {
-  #   packages = with pkgs; [
-  #     # icon fonts
-  #     material-design-icons
-  #     font-awesome
+  fonts = {
+    packages = with pkgs; [
+      # icon fonts
+      material-design-icons
+      font-awesome
 
-  #     # nerdfonts
-  #     # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/fonts/nerdfonts/shas.nix
-  #     (nerdfonts.override {
-  #       fonts = [
-  #         # symbols icon only
-  #         "NerdFontsSymbolsOnly"
-  #         # Characters
-  #         "FiraCode"
-  #         "JetBrainsMono"
-  #         "Iosevka"
-  #       ];
-  #     })
-  #   ];
-  # };
+      # nerdfonts
+      # https://github.com/NixOS/nixpkgs/blob/nixos-24.05/pkgs/data/fonts/nerdfonts/shas.nix
+      (nerdfonts.override {
+        fonts = [
+          # symbols icon only
+          "NerdFontsSymbolsOnly"
+          # Characters
+          "FiraCode"
+          "JetBrainsMono"
+          "Iosevka"
+        ];
+      })
+    ];
+  };
 }
