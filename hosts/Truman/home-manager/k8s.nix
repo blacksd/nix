e964ec03a03dcff86a315c2a4df488a03481e790
Marco Bulgarini <marco.bulgarini@hivemq.com>
2025-06-03 09:24:05 +0200

    feat: k8s argo
    Signed-off-by: Marco Bulgarini <marco.bulgarini@hivemq.com>

--- hosts/Truman/home-manager/k8s.nix
+++ hosts/Truman/home-manager/k8s.nix
@@ -1,17 +1,45 @@
 {pkgs, ...}: {
   programs = {
     k9s = {
+      hotkey = {
+        hotKeys = {
+          shift-0 = {
+            shortCut = "Shift-0";
+            description = "ArgoCD Applications (argocd namespace)";
+            command = "applications.argoproj.io argocd";
+          };
+        };
+      };
       plugin = {
         plugins = {
           view-secret = {
             shortCut = "Shift-S";
             description = "View secret (all)";
-            scopes = ["secret"];
+            scopes = ["secrets"];
             command = "sh";
             background = false;
             args = [
               "-c"
               "kubectl view-secret --context $CLUSTER --namespace $NAMESPACE --all $NAME | less"
+            ];
+          };
+          disable-auto-sync = {
+            shortCut = "Shift-Y";
+            confirm = false;
+            scopes = [
+              "applications.argoproj.io"
+            ];
+            description = "Disable ArgoCD sync";
+            command = "kubectl";
+            background = false;
+            args = [
+              "patch"
+              "applications"
+              "--namespace"
+              "$NAMESPACE"
+              "$NAME"
+              "--type=json"
+              "-p=[{\"op\":\"replace\", \"path\": \"/spec/syncPolicy\", \"value\": {}}]"
             ];
           };
         };
