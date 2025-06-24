230e54417ef58372b1ed2864979ed9957c1e0a65
Marco Bulgarini <marco.bulgarini@hivemq.com>
2025-06-24 18:41:05 +0200

    feat: k9s plugins
    Signed-off-by: Marco Bulgarini <marco.bulgarini@hivemq.com>

--- hosts/Truman/home-manager/k8s.nix
+++ hosts/Truman/home-manager/k8s.nix
@@ -12,22 +12,62 @@
       };
       plugin = {
         plugins = {
-          disable-auto-sync = {
-            shortCut = "Shift-Y";
-            confirm = false;
+          enable-auto-sync = {
+            shortCut = "s";
+            confirm = true;
             scopes = [
               "applications.argoproj.io"
             ];
-            description = "Disable ArgoCD sync";
+            description = "Enable ArgoCD auto-sync";
             command = "kubectl";
             background = false;
             args = [
+              "--context=$CLUSTER"
+              "--namespace=$NAMESPACE"
               "patch"
               "applications"
-              "--namespace $NAMESPACE"
               "$NAME"
               "--type=json"
-              "-p=[{\"op\":\"replace\", \"path\": \"/spec/syncPolicy\", \"value\": {}}]"
+              "--patch=[{\"op\":\"replace\", \"path\": \"/spec/syncPolicy\", \"value\": {\"automated\": {} }}]"
+            ];
+          };
+          disable-auto-sync = {
+            shortCut = "Shift-S";
+            confirm = true;
+            scopes = [
+              "applications.argoproj.io"
+            ];
+            description = "Disable ArgoCD auto-sync";
+            command = "kubectl";
+            background = false;
+            args = [
+              "--context=$CLUSTER"
+              "--namespace=$NAMESPACE"
+              "patch"
+              "applications"
+              "$NAME"
+              "--type=json"
+              "--patch=[{\"op\":\"replace\", \"path\": \"/spec/syncPolicy\", \"value\": {} }]"
+            ];
+          };
+          ephemeral-container = {
+            shortCut = "Shift-E";
+            confirm = false;
+            scopes = [
+              "pods"
+            ];
+            description = "Run ephemeral container on a pod";
+            command = "kubectl";
+            background = false;
+            args = [
+              "--context=$CLUSTER"
+              "--namespace=$NAMESPACE"
+              "debug"
+              "$NAME"
+              "--tty"
+              "--stdin"
+              "--image=nicolaka/netshoot"
+              "--profile=general"
             ];
           };
         };
