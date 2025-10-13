5653a47c4e04826368bcaff597f828f251c43405
Marco Bulgarini <marco.bulgarini@hivemq.com>
2025-10-13 21:31:48 +0200

    feat: claude code v2
    Signed-off-by: Marco Bulgarini <marco.bulgarini@hivemq.com>

--- hosts/Truman/home-manager/ai.nix
+++ hosts/Truman/home-manager/ai.nix
@@ -1,7 +1,19 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  nixpkgs-unstable,
+  ...
+}: let
+  pkgs-unstable = import nixpkgs-unstable {
+    system = pkgs.system;
+    config.allowUnfree = true;
+  };
+in {
   home.packages = with pkgs; [
     codex
   ];
 
-  programs.claude-code.enable = true;
+  programs.claude-code = {
+    enable = true;
+    package = pkgs-unstable.claude-code;
+  };
 }
