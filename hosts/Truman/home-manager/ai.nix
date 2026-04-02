e9b401792d2572d6cb4e6d8bcf38b16070bee5c4
Marco Bulgarini <marco.bulgarini@hivemq.com>
2026-04-02 23:32:23 +0200

    feat: live on the edge
    Signed-off-by: Marco Bulgarini <marco.bulgarini@hivemq.com>

--- hosts/Truman/home-manager/ai.nix
+++ hosts/Truman/home-manager/ai.nix
@@ -3,13 +3,13 @@
   nixpkgs-unstable,
   ...
 }: let
+  # Get packages from nixpkgs-unstable
   pkgs-unstable = import nixpkgs-unstable {
     system = pkgs.stdenv.hostPlatform.system;
     config.allowUnfree = true;
   };
 in {
-  home.packages = with pkgs; [
-    asciinema
-    asciinema-agg
+  home.packages = with pkgs-unstable; [
+    codex
   ];
 }
